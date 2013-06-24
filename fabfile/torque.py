e5c4268ad8c43ed011ed65793056881d5f30c826
Koji Tanaka <kj.tanaka@gmail.com>
2013-06-24 16:49:26 -0400

    start working torque.py

--- fabfile/torque.py
+++ fabfile/torque.py
@@ -19,13 +19,12 @@ def node_ensure(nodename,np,properties,note,gpus=0):
     with settings(warn_only = True):
         output = run('pbsnodes %s' % nodename)
     if output.return_code != 0:
-        with prefix('LANG=en_US.UTF-8'):
-            run('qmgr -c \"create node %s\"' % nodename)
-            time.sleep(3)
-            run('qmgr -c \"set node %s np = %s\"' % (nodename, np))
-            time.sleep(3)
-            run('qmgr -c \"set node %s properties = %s\"' % (nodename, properties))
-            time.sleep(3)
-            run('qmgr -c \"set node %s note = %s\"' % (nodename, note))
-            time.sleep(3)
-            run('qmgr -c \"set node %s gpus = %s\"' % (nodename, gpus))
+        run('qmgr -c \"create node %s\"' % nodename)
+        time.sleep(1)
+        run('qmgr -c \"set node %s np = %s\"' % (nodename, np))
+        time.sleep(1)
+        run('qmgr -c \"set node %s properties = %s\"' % (nodename, properties))
+        time.sleep(1)
+        run('qmgr -c \"set node %s note = %s\"' % (nodename, note))
+        time.sleep(1)
+        run('qmgr -c \"set node %s gpus = %s\"' % (nodename, gpus))

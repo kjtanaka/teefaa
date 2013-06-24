92da8ce21a5bf91026e8eefdf2c8cf8b146176e4
Koji Tanaka <kj.tanaka@gmail.com>
2013-06-24 15:57:10 -0400

    start working torque.py

---
+++ fabfile/torque.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+#
+# tfutils - installs utilities.
+#
+
+import os
+import sys
+from fabric.api import *
+from fabric.contrib import *
+from cuisine import *
+
+@task
+def node_ensure(nodename,np,properties,note,gpus=0):
+    ''':nodename,np,properties,note,gpus=0
+    --- ensure node exists'''
+    env.host_string = 'i136'
+    with settings(warn_only = True):
+        output = run('pbsnodes|grep %s' % nodename)
+        print output.return_code

a0cd035eede1e95d91f54895dfe6d4cac66ccce5
Gregor von Laszewski <laszewski@gmail.com>
2013-07-16 14:09:47 -0400

    moving teefa in differnt dir

--- teefaa/api.py
+++ teefaa/api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 # -------------------------------------------------------------------------- #
-# Copyright 2010-2011, Indiana University                                    #
+# Copyright 2012-2013, Indiana University                                    #
 #                                                                            #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may    #
 # not use this file except in compliance with the License. You may obtain    #
@@ -14,10 +15,33 @@
 # See the License for the specific language governing permissions and        #
 # limitations under the License.                                             #
 # -------------------------------------------------------------------------- #
-import setuptools
+"""
+Description: Simplified Dynamic Provisioning tool in Teefaa. Installs customized images of Operating System on Bare Metal machine.  
+"""
+__author__ = 'Koji Tanaka, Javier Diaz'
 
-setuptools.setup(
-    setup_requires=['d2to1', 'pbr>=0.5,<0.6'],
-    d2to1=True)
+import os
+import sys
+import time
+import ConfigParser
+import argparse
+import string
+from fabric.api import *
+from fabric.contrib import *
 
-#    version='#:')
+def baremetal_provisioning(hostname, imagename):
+    cfgfile = '~/.teefaa/teefaa.conf'
+    cfg = ConfigParser.SafeConfigParser()
+    cfg.read(os.path.expanduser(cfgfile))
+
+    sys.path.append(cfg.get('fabric', 'path_to_fabfile'))
+    import baremetal
+
+    execute(baremetal.provisioning, hostname, imagename)
+    print imagename
+
+def main():
+    baremetal_provisioning('i2', 'india_openstack_v1')
+
+if __name__ == "__main__":
+    main()

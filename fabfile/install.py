e947379fe20c759abdaaaba4826fa9fd1af40a44
Koji Tanaka <kj.tanaka@gmail.com>
2013-10-29 11:54:24 -0400

    test updte_tftpd_conf

--- fabfile/install.py
+++ fabfile/install.py
@@ -4,12 +4,25 @@
 # tfutils - installs utilities.
 #
 
-from fabric.api import task
-from cuisine import select_package, \
-        package_ensure
+from fabric.api import \
+        task
+from cuisine import \
+        select_package, \
+        package_ensure, \
+        file_update
 
 @task
 def pxeserver():
 
     select_package('apt')
     package_ensure('tftpd-hpa syslinux')
+    file_update('/etc/default/tftpd-hpa', _update_tftpd_conf)
+
+def _update_tftpd_conf(conf):
+
+    new_conf = [
+            "# Defaults for tftpd-hpa",
+            "RUN_DAEMON=\"yes\"",
+            "OPTIONS=\"-l -s /tftpboot\""
+            ]
+    return '\n'.join(new_conf)

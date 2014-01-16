03044c8d6f7e0128afd85a9e32a49ecf93454e7c
Koji Tanaka <kj.tanaka@gmail.com>
2014-01-15 23:24:45 -0500

    adding check_ssh

--- teefaa/ssh.py
+++ teefaa/ssh.py
@@ -2,6 +2,7 @@
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 import os
+import time
 import argparse
 import subprocess
 from fabric.api import local, execute, task, hide
@@ -10,35 +11,54 @@ from .lib.common import read_config
 
 class TeefaaSsh(object):
 
+    def __init__(self):
+
+        config = read_config()
+        self.ssh_config = config['ssh_config']
+        self.hostname = config['host_config']['hostname']
+        try:
+            self.ssh_key = os.path.abspath(config['ssh_key'])
+        except:
+            self.ssh_key = None
+
     def setup(self, parser):
 
         ssh = parser.add_parser(
                 'ssh', 
                 help="ssh to destination host")
-        ssh.set_defaults(func=self.ssh)
+        ssh.set_defaults(func=self.do_ssh)
 
-    def ssh(self, args):
+    def do_ssh(self, args):
 
-        config = read_config()
-        ssh_config = config['ssh_config']
-        hostname = config['host_config']['hostname']
-        cmd = ['ssh', '-F', ssh_config, hostname]
-        try:
-            ssh_key = os.path.abspath(config['ssh_key'])
-            cmd.append('-i ' + ssh_key)
-        except:
-            ssh_key = None
-        print("\nssh to machine '{0}'...\n".format(hostname))
+        print("\nssh to machine '{0}'...\n".format(self.hostname))
+        self.check_ssh()
+        cmd = ['ssh', '-F', self.ssh_config, self.hostname]
+        if self.ssh_key: cmd.append('-i' + self.key)
         try:
             subprocess.check_call(cmd)
-            #execute(fab_ssh, ssh_config, hostname, ssh_key)
         except subprocess.CalledProcessError:
             print("SSH is disconnected...")
 
-@task
-def fab_ssh(ssh_config, hostname, ssh_key):
-    cmd = ['ssh', '-F', ssh_config, hostname]
-    if not ssh_key == None:
-        cmd.append("-i " + ssh_key)
-    with hide('running'):
-        local(' '.join(cmd))
+    def check_ssh(self):
+
+        count = 1
+        limit = 50
+        FNULL = open(os.devnull, 'w')
+        cmd = ['ssh', '-o', 'ConnectTimeout=5', '-F', self.ssh_config]
+        if self.ssh_key: cmd.append('-i' + self.key)
+        cmd.append(self.hostname)
+        cmd.append('hostname')
+        while count < limit:
+            try:
+                subprocess.check_call(cmd, stdout=FNULL, stderr=subprocess.STDOUT)
+                break
+            except subprocess.CalledProcessError:
+                print ("'{h}' is offline. Wait and retry ssh ({c}/{l})...".format(
+                    h=self.hostname,c=count,l=limit))
+                count += 1
+                time.sleep(10)
+
+
+def check_ssh():
+    ts = TeefaaSsh()
+    ts.check_ssh()
